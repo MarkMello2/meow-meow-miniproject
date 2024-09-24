356701098fd102150d159d20d8465476bbaf33eb
MarkMello2 <zKittanonS@pttep.com>
2024-09-24 15:44:39 +0700

    features: add script banner

---
+++ script/insert/insert-banners-item.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.banners("name", description, image, created_at, updated_at, deleted_at) 
+VALUES('Banner1', 'Banner1', 'banners/banner-1.jpg', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3), NULL);
+
+INSERT INTO public.banners("name", description, image, created_at, updated_at, deleted_at) 
+VALUES('Banner2', 'Banner2', 'banners/banner-2.jpg', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3), NULL);
+
+INSERT INTO public.banners("name", description, image, created_at, updated_at, deleted_at) 
+VALUES('Banner3', 'Banner3', 'banners/banner-3.jpg', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3), NULL);
