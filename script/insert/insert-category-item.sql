fb1185b784bc16333b96eecb09bb1b6d90dc981a
MarkMello2 <zKittanonS@pttep.com>
2024-09-24 19:24:15 +0700

    features: add category item

--- script/insert/insert-category-item.sql
+++ script/insert/insert-category-item.sql
@@ -1,17 +1,5 @@
-insert
-	into
-	public.categories
-("name",
-	description,
-	image,
-	created_at,
-	updated_at,
-	deleted_at)
-values(
-	'Pet supplies', 
-	'Pet supplies', 
-	'category/pet_supplies.png',
-	CURRENT_TIMESTAMP(3),
-	CURRENT_TIMESTAMP(3),
-	NULL
-);
+insert into public.categories("name", description, image, created_at, updated_at, deleted_at) 
+values('Food', 'This is Food', 'category/Animal_food.png', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3), NULL);
+
+insert into public.categories("name", description, image, created_at, updated_at, deleted_at) 
+values('Pet supplies', 'Pet supplies', 'category/pet_supplies.png', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3), NULL);
