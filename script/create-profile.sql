446828668f634932828c9472ecf305909097f462
MarkMello2 <zKittanonS@pttep.com>
2024-09-24 19:16:04 +0700

    features: add create table profile and user

--- script/create-profile.sql
+++ script/create-profile.sql
@@ -1,13 +1,13 @@
-CREATE TABLE address (
+CREATE TABLE profiles (
     id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     mobile VARCHAR(10),
-    address VARCHAR(1024),
-    type BIGINT DEFAULT 0,
-    user_id BIGINT REFERENCES users(id),
+    sex VARCHAR(1) CHECK (sex IN ('M', 'F')),
+    status VARCHAR(1) CHECK (status IN ('A', 'I')),
+    image VARCHAR(255),
+    user_id BIGINT NOT NULL REFERENCES users(id),
     created_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP(3)
 );
-
