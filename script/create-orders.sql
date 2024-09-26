4f6c1cad77d72b0f0e5ece613fc5fed1ddd62126
MarkMello2 <zKittanonS@pttep.com>
2024-09-26 15:51:08 +0700

    features: create orders table script

---
+++ script/create-orders.sql
@@ -0,0 +1,12 @@
+CREATE TABLE orders (
+    id BIGSERIAL PRIMARY KEY,
+    price FLOAT NOT NULL,
+    quantity BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    created_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP(3),
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products,
+    CONSTRAINT fk_userId FOREIGN KEY (user_id) REFERENCES users
+);
